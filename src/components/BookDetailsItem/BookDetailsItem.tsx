f5416aa6326a0e463d937f71183fae8e64c8c57a
Eduardo Cuomo <reduardo7@gmail.com>
2021-09-21 20:43:11 -0300

    touch

--- src/components/BookDetailsItem/BookDetailsItem.tsx
+++ src/components/BookDetailsItem/BookDetailsItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { TouchableOpacity, View, Image } from 'react-native';
 import styles from './styles';
-// import { goToScreen } from '../../navigation/controls';
+import { goToScreen } from '../../navigation/controls';
 import { CustomText } from '..';
 import { colors } from '../../utils/theme';
+
+const placeholderImg = require('../../assets/images/placeholder.png');
 
 interface Props {
   id: number;
@@ -11,46 +13,24 @@ interface Props {
   imageCover?: string;
 }
 
-const BookDetailsItem: React.FC<Props> = ({ title, imageCover }) => {
-  // const onPress = () => goToScreen('BookDetails', { id, title });
+const BookDetailsItem: React.FC<Props> = ({ id, title, imageCover }) => {
+  const onPress = () => goToScreen('BookDetails', { id, title });
 
   return (
-    <View style={styles.cardContainer}>
-      {imageCover && imageCover !== '' ? (
-        <>
-          <View style={styles.imageFrame}>
-            <Image source={{ uri: imageCover }} style={styles.image} />
-          </View>
-          <CustomText
-            numberOfLines={2}
-            size={14}
-            align="center"
-            color={colors.lightOrange}
-            variant="bold"
-          >
-            {title}
-          </CustomText>
-        </>
-      ) : (
-        <>
-          <View style={styles.imageFrame}>
-            <Image
-              source={require('../../assets/images/placeholder.png')}
-              style={styles.placeholderImage}
-            />
-          </View>
-          <CustomText
-            numberOfLines={2}
-            size={14}
-            align="center"
-            color={colors.lightOrange}
-            variant="bold"
-          >
-            {title}
-          </CustomText>
-        </>
-      )}
-    </View>
+    <TouchableOpacity style={styles.cardContainer} onPress={onPress}>
+      <View style={styles.imageFrame}>
+        <Image source={{ uri: imageCover || placeholderImg }} style={styles.image} />
+      </View>
+      <CustomText
+        numberOfLines={2}
+        size={14}
+        align="center"
+        color={colors.lightOrange}
+        variant="bold"
+      >
+        {title}
+      </CustomText>
+    </TouchableOpacity>
   );
 };
 
