b577f2054d13cbee5dcb620a6d0924df0918e555
Eduardo Cuomo <reduardo7@gmail.com>
2021-09-21 22:41:37 -0300

    wip

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { HomeScreen, HistoryScreen } from '../screens';
+import { HomeScreen, HistoryScreen, CharactersScreen } from '../screens';
 import { COMPONENT_NAME as HOME } from '../screens/Home/Home';
 import { COMPONENT_NAME as HISTORY } from '../screens/History/History';
+import { COMPONENT_NAME as CHARACTERS } from '../screens/Characters/Characters';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ const HomeStack = () => (
   <Stack.Navigator initialRouteName={HOME} screenOptions={{ headerShown: false }}>
     <Stack.Screen name={HOME} component={HomeScreen} />
     <Stack.Screen name={HISTORY} component={HistoryScreen} />
+    <Stack.Screen name={CHARACTERS} component={CharactersScreen} />
   </Stack.Navigator>
 );
 
