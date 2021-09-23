1f836e9d6ebcff114218475ae62647c275a96fbd
Mariano Sckerl <marianosckerl@Marianos-MacBook-Pro-2.local>
2021-09-23 11:17:16 -0300

    fix navbar

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import TabNavigator, { COMPONENT_NAME as TAB_NAVIGATOR } from './TabNavigator';
-import { BookDetailsScreen } from '../screens';
+import { BookDetailsScreen, CharacterDetailsScreen } from '../screens';
 import { COMPONENT_NAME as BOOK_DETAILS } from '../screens/BookDetails/BookDetails';
+import { COMPONENT_NAME as CHARACTER_DETAILS } from '../screens/CharacterDetails/CharacterDetails';
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => (
   <Stack.Navigator initialRouteName={TAB_NAVIGATOR} screenOptions={{ headerShown: false }}>
     <Stack.Screen name={TAB_NAVIGATOR} component={TabNavigator} />
-    <Stack.Screen name={BOOK_DETAILS} component={BookDetailsScreen} />
   </Stack.Navigator>
 );
 
